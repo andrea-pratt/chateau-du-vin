c99a1b0894ac17620e1c5a25ab48fb482d7483e3
Andrea <apratt1013@gmail.com>
2021-07-24 15:20:36 -0500

    add log in and register features

--- chateau_du_vin/chateau_du_vin_app/urls.py
+++ chateau_du_vin/chateau_du_vin_app/urls.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('rates', views.rates, name='rates'),
-    path('about', views.about_owner, name='about')
+    path('about', views.about_owner, name='about'),
+    path('register', views.register, name='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
--- chateau_du_vin/chateau_du_vin_app/views.py
+++ chateau_du_vin/chateau_du_vin_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -11,3 +15,28 @@ def rates(request):
 
 def about_owner(request):
     return render(request, 'about_owner.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            
+            if user:
+                messages.info(request, 'Thank you, for signing up!')
+                login(request, user)
+
+                messages.info(request, 'Account created successfully!')
+                return redirect('rates')
+
+            else:
+                messages.add_message(request, messages.ERROR, 'Unable to log in new user')
+        else:
+            messages.add_message(request, messages.INFO, 'Please check the data you entered')
+            # include the invalid form, which will have error messages added to it. The error messages will be displayed by the template.
+            return render(request, 'register.html', {'form': form} )
+
+    form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form} )
